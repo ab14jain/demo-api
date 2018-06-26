93bab7807c170d2d582f5c08e00051b53de70524
unknown <ab14jain@gmail.com>
2018-06-26 22:17:16 +0530

    updated api

--- index.js
+++ index.js
@@ -7,38 +7,26 @@ var app = express();
 
 app.use(cors())
 
-// app.get('/api/customers', (req, res) => {
-//     var customers = [
-//         { "id": 1, "name": "Abhishek", "age": 28 },
-//         { "id": 2, "name": "Hardik", "age": 26 },
-//         { "id": 3, "name": "Sitanshu", "age": 27 },
-//         { "id": 4, "name": "Bashir", "age": 24 },
-//         { "id": 5, "name": "Satya", "age": 22 },
-//         { "id": 6, "name": "Adarsh", "age": 24 }
-//     ]
+app.get('/api/customers', (req, res) => {
+    var customers = [
+        { "id": 1, "name": "Abhishek", "age": 28 },
+        { "id": 2, "name": "Hardik", "age": 26 },
+        { "id": 3, "name": "Sitanshu", "age": 27 },
+        { "id": 4, "name": "Bashir", "age": 24 },
+        { "id": 5, "name": "Satya", "age": 22 },
+        { "id": 6, "name": "Adarsh", "age": 24 }
+    ]
 
-//     // res.status(200).json({
-//     // 	customers
-//     // })
-// });
-
-// app.get('/', (req, res) => {
-//     // res.status(200).json({
-//     //     message: "It's working"
-//     // })
-// })
-app.get('/', function (request, response) {
-    response.send('Hello World!')
+    res.status(200).json({
+    	customers
+    })
 });
 
-app.get('/users', function (req, res) {
-    // Hard coding for simplicity. Pretend this hits a real database
-    res.json([
-        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
-        { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
-        { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
-    ]);
-});
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "It's working"
+    })
+})
 
 app.listen((process.env.PORT || 5000), () => {
     console.log("server listening port: " + PORT)
