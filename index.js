cf1eb3a94eb49acaa1b2ca2d56d3ffaf500a4ded
unknown <ab14jain@gmail.com>
2018-06-26 22:13:46 +0530

    removed hardcoded port to env.Port

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.get('/users', function (req, res) {
     ]);
 });
 
-app.listen(PORT, () => {
+app.listen((process.env.PORT || 5000), () => {
     console.log("server listening port: " + PORT)
 });
